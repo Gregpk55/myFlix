36c5092aa795ab802f8c89561d2326081616f662
Gregpk55 <122652405+Gregpk55@users.noreply.github.com>
2023-04-09 20:37:21 -0700

    create connection

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(morgan("common"));
 
 // Connect Mongoose
 //mongoose.connect('mongodb://localhost:27017/cfDB')
-mongoose.connect( process.env.CONNECTION_URI,
+mongoose.createConnection( process.env.CONNECTION_URI,
   { useNewUrlParser: true, useUnifiedTopology: true })
     .then (() => console.log('connected to mongoDB'))
     .catch ((err) => console.error(err));
