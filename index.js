c830c757f8a24288f9ca9360ca7626094fef110c
Gregpk55 <122652405+Gregpk55@users.noreply.github.com>
2023-04-16 12:56:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ app.delete(
 );
 
 // Get all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
+app.get('/movies',  async (req, res) => {
   try {
     const movies = await Movies.find();
     res.status(200).json(movies);
