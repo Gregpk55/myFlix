327fb5e18e08531025cbe392b79ddbe6286c0707
Gregpk55 <122652405+Gregpk55@users.noreply.github.com>
2023-04-19 17:34:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,24 @@ const app = express();
 const auth = require('./auth')(app);
 
 // Define middleware
-app.use(cors());
+let allowedOrigins = [
+  'http://localhost:1234',
+  'http://localhost:8080',
+  'http://localhost:27017',
+  'https://greg-kennedy-myflix.herokuapp.com'
+];
+app.use(cors({
+
+origin: (origin, callback) => {
+  if(!origin) return callback(null, true);
+  if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+    let message = 'The CORS policy for this application doesn/t allow access from origin ' + origin;
+    return callback(new Error(message ), false);
+  }
+  return callback(null, true);
+}
+}));
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
